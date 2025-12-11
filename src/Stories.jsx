9484aa1e252c1bb77fcaabe1c533d794f264ab2d
adithya519b <adithya462005@gmail.com>
2025-12-11 19:02:07 +0530

    db fix

--- src/Stories.jsx
+++ src/Stories.jsx
@@ -3,28 +3,42 @@ import { useNavigate } from 'react-router-dom';
 
 function Stories() {
   const [Storys, setStory] = useState([]);
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    fetch("/insta/db.json")
+
+    const base = process.env.NODE_ENV === "production" ? "/insta/" : "/";
+
+    fetch(base + "db.json")
       .then(res => res.json())
       .then(data => {
         console.log(data.stories);
-        setStory(data.stories);  // <-- save to state
+        setStory(data.stories);  // <-- FIX HERE
       })
       .catch(err => console.error(err));
-  }, []);  // <-- important!!
-  let tot=0;
+
+  }, []);
+
+  let tot = Storys.length;
+
   return (
-    
     <div className='stories'>
-     <div className='d-none'> {tot=Storys.length}</div> 
-      {Storys.length >0 ?(Storys.map((story) => (
-        <div className="story" key={story.id} onClick={()=>{navigate(`/stories/${story.id}/${tot}`)}}>
-          <img  className='img' src={story.profilePic} alt="profile" />
-          <p className='text-truncate ' style={{width:"70px"}}>{story.username}</p>
-        </div>
-      ))): ( <p>loading...</p> )}
+      {Storys.length > 0 ? (
+        Storys.map((story) => (
+          <div 
+            className="story" 
+            key={story.id} 
+            onClick={() => navigate(`/stories/${story.id}/${tot}`)}
+          >
+            <img className='img' src={story.profilePic} alt="profile" />
+            <p className='text-truncate' style={{ width: "70px" }}>
+              {story.username}
+            </p>
+          </div>
+        ))
+      ) : (
+        <p>loading...</p>
+      )}
     </div>
   );
 }
